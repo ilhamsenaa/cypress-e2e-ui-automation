946805776e493e27de70a637afb3901779d5fa9c
ilhamsenaaa <ilhamsenaa@gmail.com>
2024-11-10 19:18:33 +0700

    [sena] fix allure report and use allure-cypress

--- cypress/plugins/index.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable */
-
-// This function is called when a project is opened or re-opened (e.g. due to
-// the project's config changing)
-const allure = require('@shelex/cypress-allure-plugin/writer');
-module.exports = (on, config) => {
-  allure(on, config); // Add this line
-  return config;
-};
